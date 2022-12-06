738635776ca605c5a1216f35723acf965382e6fe
Sarah2603 <sarahbarreto86@gmail.com>
2022-12-06 13:16:27 -0300

    first commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+using WebApi.Context;
+
+namespace WebApi.Controllers
+{
+     [ApiController]
+     [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/contato
+        [HttpPost("CriarContato")]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+        [HttpGet("id")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return Ok(contato);
+        }
+        [HttpPut("id")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+            
+                return NotFound();
+            
+               contatoBanco.Nome = contato.Nome;
+               contatoBanco.Telefone = contato.Telefone;
+               contatoBanco.Ativo = contato.Ativo;
+               
+               _context.Contatos.Update(contatoBanco);
+               _context.SaveChanges();
+                return Ok(contatoBanco);
+        }
+        [HttpDelete("id")]
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            _context.Contatos.Remove(contato);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+    }
+
+}
+
+
+            
+        
+
+    
+
+    
+    
+    
+    
+        
+    
